a05ec4fae8b090ed0263241b9ba98f065f2957f4
Yann Thobena <thobena.yann@orange.fr>
2022-11-18 10:31:54 +0100

    feat: setup env variable for development

--- app/Header.tsx
+++ app/Header.tsx
@@ -34,19 +34,9 @@ const Header = () => {
             </Link>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a href="#feature">Stack</a></li>
-            <li><a href="#step">Méthode</a></li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+            <li><a href={`${process.env.BASE_URL}/#feature`}>Stack</a></li>
+            <li><a href={`${process.env.BASE_URL}/#step`}>Méthode</a></li>
+            <li><Link href="/articles">Blog</Link></li>
           </ul>
         </div>
         {/* Logo */}
@@ -55,19 +45,9 @@ const Header = () => {
       {/* Navbar desktop */}
       <nav className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          <li><a href="#feature">Stack</a></li>
-          <li><a href="#step">Méthode</a></li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
+          <li><a href={`${process.env.BASE_URL}/#feature`}>Stack</a></li>
+          <li><a href={`${process.env.BASE_URL}/#step`}>Méthode</a></li>
+          <li><Link href="/articles">Blog</Link></li>
         </ul>
       </nav>
       {/* Searchbar */}
