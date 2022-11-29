e3e8da8d9f3fb963c15ba696110650a793bdfbe7
Yann Thobena <thobena.yann@orange.fr>
2022-11-29 10:05:19 +0100

    feat: product detail

--- app/products/ProductsList.tsx
+++ app/products/ProductsList.tsx
@@ -33,7 +33,7 @@ async function ProductsList() {
                   <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                     {
                       category.attributes.products.data.map((product) => (
-                        <Link key={product.id} href="#" className="group">
+                        <Link key={product.id} href={`/products/${product.id}`} className="group">
                           <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                             <Image
                               src={product.attributes.image.data.attributes.url}
